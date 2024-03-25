2fd71618e9fd7cca80066cf3f982b41a0adffe86
jeff muriuki <jeff.muriuki@studet.moringaschool.com>
2024-03-25 18:17:31 +0300

    advanced functions

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers () {
+  return drivers.slice(0,2);
+ }
+ 
+
+ function returnLastTwoDrivers () {
+    return drivers.slice(2,4);
+ }
+
+ const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+   
+ function selectDifferentDrivers([], selectingDrivers) {
+   
+    return selectingDrivers;
+}
+
+selectDifferentDrivers(drivers, returnFirstTwoDrivers);
+selectDifferentDrivers(drivers, returnLastTwoDrivers);
+
+
+
+ function createFareMultiplier(integer) {
+    return function(fare) {
+        return fare * integer;
+    };
+}
+
+const fareMultiplier = createFareMultiplier(2);
+const fareQuintupler = createFareMultiplier(5);
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+
